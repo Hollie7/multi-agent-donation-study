4d62aa36e35d7885a772be7431c345d99e85aeca
Holly <60909316+Hollie7@users.noreply.github.com>
2025-07-28 07:03:53 +0000

    fix: update condition name

--- src/messages/messages.js
+++ src/messages/messages.js
@@ -3,15 +3,15 @@ import { message5BotGroups_CEF } from "./CEF";
 import { message1BotGroups_SLC } from "./SLC";
 import { message5BotGroups_PNC } from "./PNC";
 import { message5BotGroups_PNF } from "./PNF";
-import { message5BotGroups_UEC } from "./UEC";
-import { message5BotGroups_UEF } from "./UEF";
+import { message5BotGroups_UNC } from "./UNC";
+import { message5BotGroups_UNF } from "./UNF";
 
 export const messageGroupsAllBots = [
   message5BotGroups_CEC,
   message5BotGroups_CEF,
   message5BotGroups_PNC,
   message5BotGroups_PNF,
-  message5BotGroups_UEC,
-  message5BotGroups_UEF,
+  message5BotGroups_UNC,
+  message5BotGroups_UNF,
   message1BotGroups_SLC,
 ];
